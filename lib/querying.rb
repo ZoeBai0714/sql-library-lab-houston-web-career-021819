9ac599b0af59b88535ba7655d5e9ce2eb970540d
Zoe Bai <s.xue.bai@gmail.com>
2019-03-02 03:37:45 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,6 +24,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
    JOIN characters
    ON series.id = characters.series_id
+   WHERE species = 'human'
    ORDER BY COUNT('human')"
 end
 
