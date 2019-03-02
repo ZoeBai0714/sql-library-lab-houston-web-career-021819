8e847738f2f9aa0c2b55b00ea3d11ac2c737d033
Zoe Bai <s.xue.bai@gmail.com>
2019-03-02 03:03:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) FROM characters,
+  "SELECT species, COUNT(species) FROM characters
   WHERE species = 'human'"
 end
 
