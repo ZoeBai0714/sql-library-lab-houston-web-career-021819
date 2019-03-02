bdc54664f330a8bc76d876127851b943b66f6014
Zoe Bai <s.xue.bai@gmail.com>
2019-03-02 03:09:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT author.name, subgenres.name
+  "SELECT authors.name, subgenres.name
    FROM authors
    JOIN subgenres
    ON author.id = subgenres.id"
