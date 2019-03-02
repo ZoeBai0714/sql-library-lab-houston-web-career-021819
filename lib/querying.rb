7725f2520dcf812a92ded4c54615dc86844d1758
Zoe Bai <s.xue.bai@gmail.com>
2019-03-02 03:09:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT author.name, subgenres.name
    FROM authors
    JOIN subgenres
-   ON author.id = "
+   ON author.id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
